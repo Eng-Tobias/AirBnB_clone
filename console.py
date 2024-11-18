d4c83d086a0ee46c1e5c678fda3c4ccb3116a3d9
Eng-Tobias <engtobiasguya100@gmail.com>
2024-11-18 15:42:18 -0800

    Add console.py for command interpreter, update README and AUTHORS

--- console.py
+++ console.py
@@ -8,26 +8,26 @@ import sys
 
 
 def greet_user(name):
-    """
-    Greet the user by their name.
-    
-    Arguments:
-        name (str): The name of the user.
-    """
-    print(f"Hello, {name}!")
+"""
+Greet the user by their name.
+ 
+Arguments:
+name (str): The name of the user.
+"""
+print(f"Hello, {name}!")
 
 
 def main():
-    """
-    Main function to execute the program.
-    """
-    if len(sys.argv) != 2:
-        print("Usage: ./greet.py <name>")
-        sys.exit(1)
+"""
+Main function to execute the program.
+"""
+if len(sys.argv) != 2:
+print("Usage: ./greet.py <name>")
+sys.exit(1)
 
-    name = sys.argv[1]
-    greet_user(name)
+name = sys.argv[1]
+greet_user(name)
 
 
 if __name__ == "__main__":
-    main()
+main()
